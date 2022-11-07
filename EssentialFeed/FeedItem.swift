b2ca156e4ed57a53c511ab2b1df7f271cb13135e
Luane dos Santos <luane.lopes@westwing.com.br>
2022-11-07 17:50:51 -0400

    Create feed item

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Luane dos Santos Lopes on 07/11/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID?
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
